b81a5c438a99b73787d6b17a536b2ec418db71bf
karen gonzalez <gonzalezkarenayelen92@gmail.com>
2023-12-16 20:02:08 -0300

    instalacion-de-tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  // ...
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}'
+  ],
+  // ...
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  // ...
+}
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{html,js}',
+  ],
+  // ...
+}
